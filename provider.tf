be7733155824bdf41483770299990f6791a173eb
root <root@ip-172-31-84-233.ec2.internal>
2022-01-17 11:59:49 +0000

    Modification-1

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAQC5DQCKMXY6L347G"
-  secret_key = "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"
+  access_key = "xxxxx"
+  secret_key = "xxxxxx"
 }
